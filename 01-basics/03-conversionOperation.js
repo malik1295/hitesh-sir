7eb59ed6d1f512ef5ed531f714bd425b7eece1ef
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-08-29 10:37:25 +0000

    datA type conversion

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,9 @@
+// let score=45
+
+//console.log(typeof score);//number
+/*but if this is in string and we want to find value of number inside strng then we need to convert this into a number this we can do by
+*/
+let score="45"
+let valueInScore=Number(score)
+//now Number(score) will convert string score into number
+console.log();
