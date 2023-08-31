b40e67890a4e4afc5e6c461a29369e5e6fade03d
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-08-31 16:29:30 +0000

    change of type

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -3,7 +3,12 @@
 //console.log(typeof score);//number
 /*but if this is in string and we want to find value of number inside strng then we need to convert this into a number this we can do by
 */
-let score="45"
-let valueInScore=Number(score)
+// let score="45"
+// let valueInScore=Number(score)
 //now Number(score) will convert string score into number
-console.log();
+// console.log(valueInScore);//45
+/*but if the number is "45abc" then its type will be changed to number but it is not a number*/
+let score="45abc"
+let valueInScore=Number(score)
+console.log(typeof valueInScore)//number
+console.log(valueInScore)//NaN
