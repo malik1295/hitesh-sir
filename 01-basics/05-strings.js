518a24f67b0ced89f0bf18dc2f28120f9b6db376
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-02 10:27:59 +0000

    strings

---
+++ 01-basics/05-strings.js
@@ -0,0 +1,9 @@
+//string concatination
+const name="rehman"
+const age=27
+// console.log(name + age+"profession")
+/*
+this syntax of concatination is not used so often what we used is back tics method or tempalate string or we say string intercolation
+*/ 
+//string intercolation(template string)
+// console.log(`my name is ${name} my age is ${age} my professon is teaching`)
