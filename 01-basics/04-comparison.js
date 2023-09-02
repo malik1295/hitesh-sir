f6be0eff25eb90f7ebff05e4382f012ba3e44dc4
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-02 05:47:44 +0000

    comparisons

--- 01-basics/04-comparison.js
+++ 01-basics/04-comparison.js
@@ -1,3 +1,12 @@
+//these comparisons have no problem
+/*
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2<=1)
+console.log(2==1)
+console.log(2!=1)
+*/
 // console.log("2">1)//true
 // console.log("02">1)//true
 /*see the result even though the 2 is a string but java script some how convert this into numberand compare it
@@ -5,10 +14,10 @@ but in typescript this is not allowed to compare two different datatypes
 */
 // to avoid this we can use === a kind of strict check
 /*
-this also check data types so now 
+this(===) also check data types so now 
  */
-console.log("2"==2)//true
-console.log("2"===2)//false
+// console.log("2"==2)//true
+// console.log("2"===2)//false
 //see for null
 console.log(null>0)//false
 console.log(null==0)//false
