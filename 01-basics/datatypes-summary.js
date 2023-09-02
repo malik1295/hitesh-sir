24d5134a553513c998960b4fe373497f1d5567a4
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-02 06:08:58 +0000

    data type summary

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,15 @@
+/*in javascript type(means integer or float or bolean etc) of data is not told javascript understand it by itself
+*/ 
+/* there are two types of data that how data is stored and accessed 
+1.premittive
+2.non premitive or referance type
+*/
+/* #premitive
+===>these are call by value data types 
+===> 7 types=> string,number,boolean,null,undefined,symbol,bigInt
+*/
+/* #referance type or non premitive
+these are the values whose referance is allocated to you in memory
+==> types =>arrays,objects,functions
+*/
+
