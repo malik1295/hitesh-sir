fe8d6bac9e8a69f7081e55796ba6c78d03f2cfd0
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-03 16:06:46 +0000

    strings nethods

--- 01-basics/05-strings.js
+++ 01-basics/05-strings.js
@@ -3,7 +3,28 @@ const name="rehman"
 const age=27
 // console.log(name + age+"profession")
 /*
-this syntax of concatination is not used so often what we used is back tics method or tempalate string or we say string intercolation
+this syntax of concatination is not used so often what we used is back tics method or tempalate string or we say string interpolation
 */ 
-//string intercolation(template string)
+//string interpolation(template string)
 // console.log(`my name is ${name} my age is ${age} my professon is teaching`)
+/* another way to declare a string is by declaring a varible and then usin new String()
+this is very usefull way
+*/
+const myName=new String('Rehman')
+/* you will see interesting thing if u run this in browser sonsole 
+it gives you string in key value pair as
+
+String {'Rehman'}
+0:"R"
+1:"e"
+2:"h"
+3:"m"
+4:"a"
+5:"n"
+length: 6
+[[Prototype]]:String
+[[PrimitiveValue]]:"Rehman
+
+
+there are lot of methods in prortype if u open it so we must have to master these mthods as much as possible
+*/
