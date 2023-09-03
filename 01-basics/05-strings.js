c6c84fe2c84f5e7e912ac706b69bcf4ed109ee16
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-03 19:07:13 +0000

    string methods end

--- 01-basics/05-strings.js
+++ 01-basics/05-strings.js
@@ -1,7 +1,9 @@
-//string concatination
+//****strings are most powerfull things must grabb it from root ****************//
+//**********string concatination*******//
 const name="rehman"
 const age=27
 // console.log(name + age+"profession")
+//**************template string************//
 /*
 this syntax of concatination is not used so often what we used is back tics method or tempalate string or we say string interpolation
 */ 
@@ -30,19 +32,54 @@ there are lot of methods in prortype if u open it so we must have to master thes
 */
 //to access keys we can do this
 // console.log(myName[0]);//R
+//*********length*************//
 //to check length
 // console.log(myName.length);//6
+//************toUpperCase*********//
 // TO CONVERT INTO upper case
 // console.log(myName.toUpperCase());//REHMAN
+//**********charAt()***************//
 // to find what character is at certain position we use CharAt()
 // console.log(myName.charAt(2));//h
+//************indexOf***********//
 //for the position of character use indexOf() as reverse of above method
 // console.log(myName.indexOf('h'));//2
+//************substring*********//
 // to devide a string into a substring we can use this
 // console.log(myName.substring(0,3));//Reh
-//note that it does not include the chracter at 3 also negative values canno be passed
+//note that it does not include the chracter at 3 also negative values cannot be passed
+//*********slice************//
 //to cut a portion of string we use slice method as 
 // console.log(myName.slice(0,4));//Rehm
 //again value at 4th index is not include
 //we can use negative value in slice also
-console.log(myName.slice(-6,4));//Rehm
+// console.log(myName.slice(-6,4));//Rehm
+// *************trim()**************//
+//this is used to remve spaces around the string for example
+// const newName="  rehman malik  " //spaces added
+// console.log(newName)//nmae with spaces around
+// console.log(newName.trim());//without spaces
+//*************replace**************//
+//to replace some thing in string
+/*some time we need to replace something in string for example in url of something a person gives space the browser converts it into %20 so to replace this with _ we can use replace ('%20','_')
+*/
+const url="https://rehman.com/rehman%20malik"
+// console.log(url.replace('%20','_'));
+//this gives 'https://rehman.com/rehman_malik'
+
+//***********includes*************//
+/*
+this is used to check whether something included in string or not by giving true or false see
+*/
+// console.log(url.includes('rehman'));//true
+// console.log(url.includes('baba'));//false
+//*********split()**********************//
+/*
+this is used to convert string into array[] based on something like spaces or dashes- or anything for this u need
+*seperater
+*limit 
+see
+*/
+const newName1="abd-ur-rehman-malik"
+console.log(newName1.split('-'));
+
