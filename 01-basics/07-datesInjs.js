cb4a73ac9dac57a23db9d7559d49ffaf0f3bc520
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-04 11:44:35 +0000

    date andtime

--- 01-basics/07-datesInjs.js
+++ 01-basics/07-datesInjs.js
@@ -3,7 +3,20 @@ let myDate=new Date()
 // console.log(myDate);
 //this gives very difficult resul of date which is difficult to understand
 //lets convert this into string
-console.log(myDate.toString());
+// console.log(myDate.toString());
 /*
 this is more readable as month and days are clear in it 
 */
+//there are other methods
+// console.log(myDate.toDateString());
+//******Mon Sep 04 2023
+// console.log(myDate.toISOString());
+//******2023-09-04T11:26:41.597Z
+//console.log(myDate.toLocaleDateString());
+//*****9/4/2023
+// console.log(myDate.toLocaleTimeString());
+//******11:30:10 AM
+// console.log(myDate.toLocaleString());
+//*****9/4/2023, 11:31:18 AM
+console.log(myDate.toJSON());
+//*****2023-09-04T11:32:53.033Z 
