fd921643b4295e448febd869e4a4df75d4d5c653
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-04 10:29:45 +0000

    math

--- 01-basics/06-Nums-and-Math.js
+++ 01-basics/06-Nums-and-Math.js
@@ -30,8 +30,8 @@ this will fix the number of significant digits in the given number
 *this return string containing numbers either in exponential or fixed point notation with a specified number of digits 
 */
 const otherNumber= 23.8962
-console.log(otherNumber.toPrecision(3));//23.9
-/*se if we have number like 1123.7483 but we give precision value 3 this still give three numbers in digits but with exponentil notation so be care full while giving the range 
+// console.log(otherNumber.toPrecision(3));//23.9
+/*se if we have number like 1123.7483 but we give precision value 3 this still give three numbers in digits but with exponential notation so be care full while giving the range 
 */ 
 const otherNumber1= 1123.7483
 // console.log(otherNumber1.toPrecision(3));//1.12e+3
@@ -40,5 +40,15 @@ const otherNumber1= 1123.7483
 this is used when there re lot of zeros in the numbers or u find it difficult to read the numbers then this converts the number in comma notaion i.e in million ,billion system normally  called US standards
 */
 const number1=100000000000;
-console.log(number1.toLocaleString());
+// console.log(number1.toLocaleString());
 //100,000,000,000
+//*****************maths****************//
+// console.log(Math);//object [Math]{}
+/*
+if u do this in browser console you will find lot of properties related to this math object we see some of these here
+*/
+//********abs()=>for absolute value*****//
+// console.log(Math.abs(-4));//4
+//********round()=>for roundoff ********//
+console.log(Math.round(6.8));//7
+//you see this is 
