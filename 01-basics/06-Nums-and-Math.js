556c2eceabe7ee5d603967f77007c66022eefc44
malik1295 <malik91295@gmail.com>
2023-09-04 10:50:57 +0500

    nums and math

---
+++ 01-basics/06-Nums-and-Math.js
@@ -0,0 +1,7 @@
+const score=400;
+//here javascript will automatically detect that its type is numberbur some time w have to specify first 
+//in type script it is mendatory to specify type
+//****************using new keyword*******//
+const balance=new Number(230);
+//here it is garented that type is number
+console.log(balance);
