7d1841173cd108db69682d3c555516250d59492a
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-04 11:21:43 +0000

    date and time

---
+++ 01-basics/07-datesInjs.js
@@ -0,0 +1,9 @@
+//Dates
+let myDate=new Date()
+// console.log(myDate);
+//this gives very difficult resul of date which is difficult to understand
+//lets convert this into string
+console.log(myDate.toString());
+/*
+this is more readable as month and days are clear in it 
+*/
