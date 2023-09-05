e80d28f1b7b72b2ec9e1c21d317fb08e9afdfde8
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-05 17:26:16 +0000

    objects tart

---
+++ 02-basics/03-objects.js
@@ -0,0 +1,17 @@
+//objects are formed by
+/*
+ ==>singelton
+ ==>constructor method
+ ====== object.creat
+ ==>object literals
+ */
+// ==>object literals
+const jsUser={
+    name:"Rehman",
+    age:25,
+    field:"webdev",
+    email:"whymail@dontknow",
+    isLoggedIn:false
+    lastLoginDays:["monday","friday"]
+}
+// now how to access an object
