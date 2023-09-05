2d69a002b9785290f12a0c819f0ae329459fc34a
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-05 17:45:40 +0000

    objects

--- 02-basics/03-objects.js
+++ 02-basics/03-objects.js
@@ -8,10 +8,20 @@
 // ==>object literals
 const jsUser={
     name:"Rehman",
+    "full name":"Rehman Malik"
     age:25,
     field:"webdev",
     email:"whymail@dontknow",
-    isLoggedIn:false
-    lastLoginDays:["monday","friday"]
+    isLoggedIn:false,
+    lastloginDays:["monday","friday"]
 }
 // now how to access an object
+//two ways to do this 
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+/*
+[] will work if keys are given in "" becuase in object keys are by default in strings
+also . is not used in this case
+for . method "" are not needed
+**BUTT there is no way to access "full name" by . method you can only access it by [] method
+ */
