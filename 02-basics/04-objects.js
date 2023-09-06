dc93322a8087aecfeeb6ec1967de61e8e0860b19
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-06 10:24:16 +0000

    objects properties access

--- 02-basics/04-objects.js
+++ 02-basics/04-objects.js
@@ -13,10 +13,10 @@ const tinderUser={}
 ==> is non singleton object
 */
 // now give some properties to object
-const tinderUser={}
-tinderUser.id="123abc"
-tinderUser.name="naeem"
-tinderUser.isLogedIn=false
+// const tinderUser={}
+// tinderUser.id="123abc"
+// tinderUser.name="naeem"
+// tinderUser.isLogedIn=false
 // console.log(tinderUser);
 // { id: '123abc', name: 'naeem', isLogedIn: false }
 //*****object in object and access*******//
@@ -61,6 +61,41 @@ obj3=Object.assign(obj1,obj2)
 here all values of obj2 are going in obj1 treating it as trget
 but first one is preferred more because you give empty {} and want that both objects obj1 and obj2 fall in it this{} this is also a target object
 */
-const obj3=Object.assign({},obj1,obj2)
-console.log(obj3);
+// const obj3=Object.assign({},obj1,obj2)
+// console.log(obj3);
 //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+//*******using spread operator******//
+// best way is use of spread operator//
+ const obj3={...obj1,...obj2}
+//  console.log(obj3);
+ //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+ //********accsess keys or values****//
+ // very  usefull concept
+ // we have previously
+ const tinderUser={}
+ tinderUser.id="123abc"
+ tinderUser.name="naeem"
+ tinderUser.isLogedIn=false
+ console.log(tinderUser);
+ //{ id: '123abc', name: 'naeem', isLogedIn: false }
+//*****to access keys 
+console.log(Object.keys(tinderUser));
+// [ 'id', 'name', 'isLogedIn' ]
+//******to access values
+console.log(Object.values(tinderUser));
+// [ '123abc', 'naeem', false ]
+/*
+***********important result************
+see the keys and values come in array so it very usefull to store these in a variable and then run methods lke loops  to access all these keys or values
+*/
+//********.entries()**********//
+console.log(Object.entries(tinderUser));
+/*
+==>returns array in which all key value pairs become seperate array 
+ [ [ 'id', '123abc' ], [ 'name', 'naeem' ], [ 'isLogedIn', false ] ]
+*/
+//**********hasOwnProperty***********//
+/*
+this we can use to check that whether the object has the specified propert it returns true if property exist otherwise false
+*/
+console.log(tinderUser.hasOwnProperty("isLogedIn"));//true
