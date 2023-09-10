efeb00e4fa1a2a918cc8994ed180ebe5b9bb6b49
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-10 05:59:33 +0000

    about this

--- 03-basics/03-arraowfunction.js
+++ 03-basics/03-arraowfunction.js
@@ -8,14 +8,14 @@ now if u want to access username dont write ${username}
 ==> as here current context is object so to reffer this current context use
 => ${this.username} 
 */
-const user={
-    username:"Newton",
-    price:999,
-    welcomeMessage: function(){
-        console.log(`${this.username},welcome to website`);
-        console.log(this);
-    }
-}
+// const user={
+//     username:"Newton",
+//     price:999,
+//     welcomeMessage: function(){
+//         console.log(`${this.username},welcome to website`);
+//         console.log(this);
+//     }
+// }
 //now to print message
 // console.log(user.welcomeMessage());
 // Newton,welcome to website
@@ -43,8 +43,22 @@ undefined
 // see it gives u both context one before changibg username and one after that means this always reffer to curent context
 // this also shows that consle.log(this) holds very important information
 //******now if we write console.log(this) outside of object and commint other console we get **************//
-console.log(this);
+// console.log(this);
 //{}
 /*
 so in node environment we get {} empty object from console.log(this); but if you writein browser console then you get more out of this because in browser window is global object so that is current contextthere but here it is empty object
  */
+// will ths work in function see
+//  function one(){
+//   console.log(this);
+// }
+// one();
+//see now it gives a lot of information 
+// but if u give username and print using this in function can we do this see?
+function one(){
+  username="ASAD"
+  console.log(this.username);
+}
+one();
+//ASAD its working
+//******Arrow functon+++++ ()=>{} ****//
