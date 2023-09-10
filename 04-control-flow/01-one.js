24127bf1ec3b0d7ec2dcc0d35bdd84b99f492703
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-10 16:21:42 +0000

    control flow and if statement

---
+++ 04-control-flow/01-one.js
@@ -0,0 +1,3 @@
+//******* about if statement************//
+//==>> syntax: if(condition){}
+// now the code inside {} will run if the condition inside() is true otherwise the code will never enter in{}
