bb75fa067b3ea8e9efa9a256b930efc307ac3a83
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-11 17:27:57 +0000

    switch staement

---
+++ 04-control-flow/02-switch.js
@@ -0,0 +1,33 @@
+//****switch syntax*****//
+/*
+here key means the variable and value means its different values which are being checked 
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+//EXAMPLE
+const month=3
+switch (month) {
+    case 1:
+        console.log("Januarry");
+        break;
+    case 2:
+        console.log("Februry");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("not valid month");
+        break;
+}
+// ==>> March
