62bcd910f654745486428c21bd45d8abe63ed422
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-11 05:46:34 +0000

    control flow and if statement

--- 04-control-flow/01-one.js
+++ 04-control-flow/01-one.js
@@ -1,3 +1,18 @@
+/*
+   *****Importance of Control Flow******
+ the need of the control flow is that we dont want to run thw whole code everytime we want that some piece of code run for some condition and for some other condition this code should not run and this time another piece of code should run thats why we are interseted in control flow
+*/
+/*
+  ******comparison operators******
+===>>>  == for equality check
+===>>>   >,<,  for less or greater
+===>>>  >=,<= for greater and equal and less and equal
+===>>>  === strict equality check, this is used in equality check when datatype is also to be checked
+===>>>  != not equal operator
+===>>>  !== strictly not equal operator
+===>>>  && AND operator
+===>>>   || OR operator
+*/
 //******* about if statement************//
 //==>> syntax: if(condition){}
 // now the code inside {} will run if the condition inside() is true otherwise the code will never enter in{}
