eb8cb94d8e800d9954cdce5e821cf4689b821940
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-12 06:16:25 +0000

    for of loop

---
+++ 05-itterations/03-forin-forof-foreach.js
@@ -0,0 +1,15 @@
+// higher order array loops specially designed for arrays 
+
+// ******for of loop
+// no condition needed
+// no increment 
+// it check by itself where to end and whem to end 
+// only declare variable and then specify of which this variable is 
+ const arr=[1,2,3,4,5]
+ for (const num of arr) {
+    console.log(num);
+ }
+// this vill print the values of arr at each index 
+// see this is way more simple than for loop and while do while loop 
+
+//
