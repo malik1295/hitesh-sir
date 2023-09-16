868c2e2db1da8ac06fbba39a57909a5ac97a5b26
ABD UR REHMAN <140367222+malik1295@users.noreply.github.com>
2023-09-16 05:45:39 +0000

    access keys and values to itrate

--- 02-basics/04-objects.js
+++ 02-basics/04-objects.js
@@ -134,7 +134,46 @@ moreover if you think courseinstructor is big name you can give a short name and
 // const {courseInstructor:instructor}=course
 // console.log(instructor);
 // Hitesh sir
+// someother methods which will work while we iterate objects(if possible)
+/*
+Built-in methods
+==>>The Object.keys() method
+The Object.keys() method receives an object as its parameter. Remember, this object is the object you want to loop over. It's still too early to explain how you'll loop over the object itself; for now, focus on the returned array of properties when you call the Object.keys() method.
 
+Here's an example of running the Object.keys() method on a brand new car2 object:
+*/
+// const car2 = {
+//     speed: 200,
+//     color: "red"
+// }
+// console.log(Object.keys(car2)); 
+// //==>> ['speed','color']
+/*
+So, when I run Object.keys() and pass it my car2 object, the returned value is an array of strings, where each string is a property key of the properties contained in my car2 object.
+*/
+/*
+==>>>The Object.values() method
+Another useful method is Object.values()
+*/
+// const car3 = {
+//     speed: 300,
+//     color: "yellow"
+// }
+// console.log(Object.values(car3)); 
+//==>> [300, 'yellow']
+/*
+===>>>The Object.entries() method
+Finally, there's another useful method, Object.entries(), which returns an array listing both the keys and the values. 
+*/
+// const car4 = {
+//     speed: 400,
+//     color: 'magenta'
+// }
+// console.log(Object.entries(car4));
+//==>>> [ ['speed', 400], ['color', 'magenta'] ]
+/*
+This time, the values that get returned are 2-member arrays nested inside an array. In other words, you get an array of arrays, where each array item has two members, the first being a property's key, and the second being a property's value
+*/
 //**********about JSON API*************//
 //API apna kam kisi aur p dalna
 /*
